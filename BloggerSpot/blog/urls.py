d5cc8edd0fddb3cbaed743bc61b7e039ba5d6728
Amogh Shukla <amoghshukla548@gmail.com>
2025-02-28 00:35:55 +0530

    Second commit

--- BloggerSpot/blog/urls.py
+++ BloggerSpot/blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import home, expanded_blog
+from .views import home, expanded_blog, about, contact
 
 urlpatterns = [
     path('', home, name='home'),
     path('post/<int:post_id>/', expanded_blog, name='expanded_blog'),
+    path('about/', about, name='about'),
+    path('contact/', contact, name='contact'),
 ]
--- BloggerSpot/blog/views.py
+++ BloggerSpot/blog/views.py
@@ -8,3 +8,9 @@ def home(request):
 def expanded_blog(request, post_id):
     post = get_object_or_404(Post, id=post_id)
     return render(request, 'blog/expanded_blog.html', {'post': post})
+
+def about(request):
+    return render(request, 'blog/About.html')
+
+def contact(request):
+    return render(request, 'blog/Contact.html')
