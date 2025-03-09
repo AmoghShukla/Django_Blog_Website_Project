0c16306ed420d9446691f5d4f656f5050ca36058
Amogh Shukla <110371842+AmoghShukla@users.noreply.github.com>
2025-03-09 12:32:17 +0530

    Update urls.py

--- BloggerSpot/BloggerSpot/urls.py
+++ BloggerSpot/BloggerSpot/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for BloggerSpot project.
+URL configuration for BloggerSpot Project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
